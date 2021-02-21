c5df6ef809861bd2aa4067b2de0399e6a910053b
rafalkapusta <rfkapusta@gmail.com>
2021-02-21 11:15:54 +0100

    added notification

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ import ColorSelector from './components/ColorSelector';
 function App() {
   return (
     <>
+      <div className="notification is-warning has-text-centered">
+        Type CSS color name into input field and chose color from autocomplete list with click. Clicking on the save button enables to save up to 10
+        colors. <br />
+        Project is ongoing and new features are continuously implemented. Arrow keys and enter <strong>are not</strong> supported for chosing color at
+        the moment.
+      </div>
       <ColorSelector />
     </>
   );
